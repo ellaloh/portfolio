9302aa827a2babf3ac67cd4a8432dccf02385ffb
elponi <76078253+elponi@users.noreply.github.com>
2021-03-11 15:57:05 -0800

    add hamburger menu (HTML, CSS, JS)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
-console.log("Hi, my name is Ella")
-
-// Numbers
-console.log('Numbers lesson:');
-console.log('-');
-var numberInteger = 456;
-var numberInteger2 = -345; // This is negative integer
-var numberFloat = 50.25;
-
-console.log(numberInteger); // try console.logging the next two numbers
-console.log('456'); // try it out
-console.log('I\'m feeling kinda blank :(');
-
-// Objects
-console.log('---------------');
-console.log('Objects lesson');
-console.log('-');
-
-var person = {
-  name: "Ella",
-  age: 19
-};
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
