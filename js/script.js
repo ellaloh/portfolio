802b5aa5cf82df10c835c0c969434b8e3536baba
elponi <76078253+elponi@users.noreply.github.com>
2021-03-31 08:01:56 -0700

    add pop-up to user stories

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ function menuToggle () {
     x.className = 'navtoggle'
   }
 }
+
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
