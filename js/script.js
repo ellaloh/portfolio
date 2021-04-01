5ecdcf21a18ef4de9f9a2b0a21e06b31299ea0f4
elponi <76078253+elponi@users.noreply.github.com>
2021-04-01 08:07:27 -0700

    add modal to visual rep picture

--- js/script.js
+++ js/script.js
@@ -7,10 +7,35 @@ function menuToggle () {
   }
 }
 
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+// Get the modal
+var modal = document.getElementById('myModal')
+var modalImg = document.getElementById('modalImage')
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0] // first close
+
+// When the user clicks the button, open the modal
+/* btn.onclick = function() {
+  modal.style.display = "block";
+}*/
+
+function showModalWithImage (image){
+  modal.style.display = "block";
+  modalImg.src = image;
+  captionText.innerHTML = this.alt;
 }
-</script>
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
